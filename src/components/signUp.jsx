b9beb3057f2d3a48e9ac8d721414a804748b0d00
Ashish Jadhav <ashishjadhav6923@gmail.com>
2024-08-11 14:15:13 +0530

    added formik with yup in sign in page

--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -1,26 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "../assets/BlogVerse Logo.svg";
-import axios from "axios"; // Import axios
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+
 const SignUp = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post("http://localhost:5000/api/register", {
-        username,
-        password,
-      });
-      console.log(response.data);
-      if (response.status === 201) {
-        navigate("/logIn"); // Redirect to home page if status is 201
-      }
-    } catch (error) {
-      console.error("There was an error!", error.response.data);
-    }
-  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .required("Password is required"),
+  });
+
   return (
     <section className="bg-gray-5">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -36,104 +32,116 @@ const SignUp = () => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Create an account
             </h1>
-            <form
-              className="space-y-4 md:space-y-6"
-              action="#"
-              onSubmit={handleRegister}
+            <Formik
+              initialValues={{
+                username: "",
+                password: "",
+              }}
+              validationSchema={validationSchema}
+              onSubmit={async (values, { setErrors }) => {
+                try {
+                  const response = await axios.post(
+                    "http://localhost:5000/api/register",
+                    {
+                      username: values.username,
+                      password: values.password,
+                    }
+                  );
+                  console.log(response.data);
+                  if (response.status === 201) {
+                    navigate("/logIn");
+                  }
+                } catch (error) {
+                  if (error.response && error.response.data) {
+                    const formikErrors = {};
+
+                    // Set the general error message returned by the API
+                    formikErrors.general = error.response.data.message;
+
+                    setErrors(formikErrors);
+                  } else {
+                    console.error("There was an error!", error.message);
+                  }
+                }
+              }}
             >
-              <div>
-                <label
-                  //   for="email"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  onChange={(e) => setUsername(e.target.value)}
-                  value={username}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                  placeholder="name@company.com"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  //   for="password"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Password
-                </label>
-                <input
-                  value={password}
-                  type="password"
-                  name="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                  required=""
-                />
-              </div>
-              {/* <div>
-                <label
-                  for="confirm-password"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Confirm password
-                </label>
-                <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                  required=""
-                />
-              </div> */}
-              {/* <div className="flex items-start"> */}
-              {/* <div className="flex items-center h-5">
-                  <input
-                    id="terms"
-                    aria-describedby="terms"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-30"
-                    required=""
-                  />
-                </div> */}
-              {/* <div className="ml-3 text-sm">
-                  <label
-                    for="terms"
-                    className="font-light text-gray-500"
-                  >
-                    I accept the{" "}
-                    <a
-                      className="font-medium text-primary-600 hover:underline"
-                      href="#"
+              {({ errors, touched }) => (
+                <Form className="space-y-4 md:space-y-6">
+                  <div>
+                    <label
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                      htmlFor="username"
                     >
-                      Terms and Conditions
+                      Your email
+                    </label>
+                    <Field
+                      className={`bg-gray-50 border ${
+                        errors.username && touched.username
+                          ? "border-red-500"
+                          : "border-gray-300"
+                      } text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                      id="username"
+                      name="username"
+                      autoComplete="username"
+                      placeholder="username"
+                    />
+                    {errors.username && touched.username ? (
+                      <div className="text-red-500 text-sm mt-1">
+                        {errors.username}
+                      </div>
+                    ) : null}
+                  </div>
+                  <div>
+                    <label
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                      htmlFor="password"
+                    >
+                      Password
+                    </label>
+                    <Field
+                      className={`bg-gray-50 border ${
+                        errors.password && touched.password
+                          ? "border-red-500"
+                          : "border-gray-300"
+                      } text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      placeholder="••••••••"
+                    />
+                    {errors.password && touched.password ? (
+                      <div className="text-red-500 text-sm mt-1">
+                        {errors.password}
+                      </div>
+                    ) : null}
+                  </div>
+
+                  {/* Display general error */}
+                  {errors.general && (
+                    <div className="text-red-500 text-sm mt-1">
+                      {errors.general}
+                    </div>
+                  )}
+
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  >
+                    Create an account
+                  </button>
+                  <p className="text-sm font-light text-gray-500">
+                    Already have an account?{" "}
+                    <a
+                      href="/logIn"
+                      className="font-medium text-primary-600 hover:underline"
+                    >
+                      Login here
                     </a>
-                  </label>
-                </div> */}
-              {/* </div> */}
-              <button
-                type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Create an account
-              </button>
-              <p className="text-sm font-light text-gray-500">
-                Already have an account?{" "}
-                <a
-                  href="#"
-                  className="font-medium text-primary-600 hover:underline"
-                >
-                  Login here
-                </a>
-              </p>
-            </form>
+                  </p>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
