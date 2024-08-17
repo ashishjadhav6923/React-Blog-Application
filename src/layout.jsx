5b9da1a19b14dc874b68d8cf492309a58f6b1d89
Ashish Jadhav <ashishjadhav6923@gmail.com>
2024-08-17 22:05:24 +0530

    added new scrollbar with normal css and removed library used for custom scrollbar

--- src/layout.jsx
+++ src/layout.jsx
@@ -3,28 +3,13 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import { Outlet } from "react-router-dom";
 import LogInContextProvider from "./context/logInContext";
-import SimpleBar from "simplebar-react";
-import "simplebar-react/dist/simplebar.min.css";
 
 const Layout = () => {
   return (
     <LogInContextProvider>
-      <div className="hidden lg:block">
-        {" "}
-        {/* Hide on small screens */}
-        <SimpleBar style={{ maxHeight: "100vh" }}>
-          <Header />
-          <Outlet />
-          <Footer />
-        </SimpleBar>
-      </div>
-      <div className="lg:hidden">
-        {" "}
-        {/* Visible on small screens */}
-        <Header />
-        <Outlet />
-        <Footer />
-      </div>
+      <Header />
+      <Outlet />
+      <Footer />
     </LogInContextProvider>
   );
 };
