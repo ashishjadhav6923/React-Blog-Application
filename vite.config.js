8b52b350d4b380d0c5940dcccc8c241ddfac0653
Ashish Jadhav <ashishjadhav6923@gmail.com>
2024-09-24 03:14:59 +0530

    fuck it

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Configuration for building the project
-  build: {
-    outDir: 'dist', // Ensure the output directory matches what Vercel expects
-    rollupOptions: {
-      input: 'index.html', // Specify the entry point
-    },
-  },
-  // Proxy settings for local development
-  server: {
-    proxy: {
-      '/api': 'https://blogverseaj.vercel.app',
-    },
+  server:{
+    proxy:{
+      '/api':'http://blogverseaj.vercel.app'
+    }
   },
   plugins: [react()],
-  // Set the base for assets
-  base: '/', // Ensure this is set for proper asset loading
-});
+})
