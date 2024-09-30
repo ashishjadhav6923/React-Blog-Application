3b8f5b0ac184b6e7cea91a9e6a87b0328fae88c2
Ashish Jadhav <ashishjadhav6923@gmail.com>
2024-09-30 22:23:03 +0530

    user is getting automatically logged in if he has accessToken

--- src/layout.jsx
+++ src/layout.jsx
@@ -6,10 +6,30 @@ import axios from "axios";
 import { useUserContext } from "./context/userDataContext";
 
 const Layout = () => {
-  const { setloginSuccess } = useUserContext();
+  const { setloginSuccess, setUserData } = useUserContext();
   const tryLoginWithJWT = async () => {
     try {
-    } catch (error) {}
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_PATH}/api/user/loginWithJWT`,
+        {},
+        { withCredentials: true }
+      );
+      console.log("response after logIn with accessToken :", response.data);
+      if (response.status === 200) {
+        setUserData((prevState) => ({
+          ...prevState,
+          name: response.data.user.name,
+          username: response.data.user.username,
+          email: response.data.user.email,
+          profession: response.data.user.profession,
+          img: response.data.user.img,
+          blogs: response.data.user.blogs,
+        }));
+        setloginSuccess(true);
+      }
+    } catch (error) {
+      console.error("There was an error!", error.message);
+    }
   };
   useEffect(() => {
     tryLoginWithJWT();
